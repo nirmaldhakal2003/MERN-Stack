c3c2062d1718b8e5fd223b3e08f3e0981be39e1e
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2024-08-09 21:07:02 +0545

    json ko formate ma message pathuni

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.get("/",(req,res)=>{
+  res.json({
+    message : "This is Home Page"
+  })
+})
+
 app.listen(3000,()=>{
     console.log('NodeJs project has started')
 })
