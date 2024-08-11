f71ddbba71c2cd78bfa16d722085a63718d96a2c
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2024-08-11 20:57:53 +0545

    ....

--- app.js
+++ app.js
@@ -6,6 +6,11 @@ app.get("/",(req,res)=>{
     message : "This is Home Page"
   })
 })
+app.get("/about",(req,res)=>{
+    res.json({
+        message: "This is about"
+    })
+})
 
 app.listen(3000,()=>{
     console.log('NodeJs project has started')
