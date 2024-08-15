d8064afacd78b59ecf88575069663cd1dd8410d0
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2024-08-15 21:16:49 +0545

    mern Day 9 class

--- app.js
+++ app.js
@@ -2,19 +2,22 @@ require('dotenv').config()
 const express = require('express')
 const connectToDatabase = require('./database')
 const app = express()
+app.use(express.json())
 
 connectToDatabase()
 
 
 app.get("/",(req,res)=>{
-  res.json({
+  res.status(200).json({
     message : "This is home page "
   })
 })
-app.get("/about",(req,res)=>{
-    res.json({
-        message: "This is about"
-    })
+
+app.post("/blog",(req,res)=>{
+  console.log(req.body)
+  res.status(200).json({
+    message: "Blog API hit successfully"
+  })
 })
 
 app.listen(process.env.PORT,()=>{
---
+++ model/blogModel.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongose')
+const Schema = mongoose.Schema
+
+
+const blogSchema = new Schema({
+    title : {
+        type : String,
+        unique : true
+    },
+    subtitle : {
+        type : String
+    },
+    description : {
+        type : Text
+    },
+    image : {
+        type: String
+    }
+})
+
+const Blog = mongoose.model('Blog', blogSchema)
+module.exports = Blog
+
+
