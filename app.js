72f242c4aef90a9dc5f1dfc660c683ea7853fdfa
nirmaldhakal2003 <nirmalbsccsit2@gmail.com>
2024-08-20 21:14:10 +0545

    file upload garni wala gare ko

--- app.js
+++ app.js
@@ -1,11 +1,12 @@
 require('dotenv').config()
 const express = require('express')
 const connectToDatabase = require('./database')
-const Blog = require('./model/blogModel')
-
 
 const app = express() 
 app.use(express.json())
+const {multer,storage} = require('./multer/multerConfig')
+const Blog = require('./model/blogModel')
+const upload = multer({storage : storage })
 
 connectToDatabase()
 
@@ -15,26 +16,38 @@ app.get("/",(req,res)=>{
     })
 })
 
-app.post("/blog",async (req,res)=>{
-    const {title,description,image,subtitle} = req.body 
-    if(!title || !description|| !subtitle || !image){
-      return res.status(400).json({
-        message: "please provide the data"
-      })
-    }
-    await Blog.create({
-       title :  title, 
-       description : description, 
-       subtitle : subtitle, 
-       image : image
-    })
+app.post("/blog",upload.single('image'), async (req,res)=>{
+   const {title,subtitle,description} = req.body 
+   const filename = req.file.filename 
+   console.log(req.body)
+   if(!title || !subtitle || !description){
+        return res.status(400).json({
+            message : "Please provide title,subtitle,description"
+        })
+        
+   }
+   await Blog.create({
+    title : title, 
+    subtitle : subtitle, 
+    description : description, 
+    image : filename
+   })
+    
     res.status(200).json({
         message : "Blog api hit successfully"
     })
 })
 
+app.get("/blog",async (req,res)=>{
+   const blogs =  await Blog.find() // returns array
+   res.status(200).json({
+    message : "Blogs fetched successfully", 
+    data : blogs
+   })
+})
+
+app.use(express.static('./storage'))
+
 app.listen(process.env.PORT,()=>{
     console.log("NodeJs project has started")
 })
-
-
